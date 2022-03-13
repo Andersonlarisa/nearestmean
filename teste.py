40f45c76c0443ab52e5e3a7a95be73dfed354fe1
Andersonlarisa <100856268+Andersonlarisa@users.noreply.github.com>
2022-03-13 18:20:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,15 +8,26 @@ from sklearn.datasets import make_blobs
 import numpy as np
 from nearestmean import nmc
 import matplotlib.pyplot as plt
+import streamlit as st
 
-x, y = make_blobs(n_samples=100, centers=5, n_features=2,cluster_std=0.4,
-                  random_state=0)
+st.title('Exemplo Nearest Mean')
+
+qtd_Amostras=st.sidebar.number_input('qtd Amostras',value=100,min_value=2)
+qtd_Classes=st.sidebar.number_input('qtd Classes',value=3,min_value=2)
+desvio=st.sidebar.number_input('Desvio Padrão',value=0.4,min_value=0.0)
+random_state=st.sidebar.number_input('Random State',value=0,min_value=0)
+
+
+x, y = make_blobs(n_samples=qtd_Amostras, centers=qtd_Classes, n_features=2,cluster_std=desvio,
+                  random_state=random_state)
 
 
 clf=nmc()
 clf.fit(x,y)
 
-x_test=np.array([[1,1],[-1,6]])
+x1=st.sidebar.number_input('Amostra eixo x',value=1)
+y1=st.sidebar.number_input('Amostra eixo y',value=1)
+x_test=np.array([x1,y1],ndmin=2)
 
 y_pred=clf.predict(x_test)
 
@@ -25,5 +36,5 @@ ax.scatter(x[:,0], x[:,1],c=y)
 ax.scatter(clf.cluster_mean[:,0],clf.cluster_mean[:,1],c='#000000')
 ax.scatter(x_test[:,0],x_test[:,1],c=y_pred,marker='x',vmin=y.min(),vmax=y.max())
 
-plt.show()
-
+#plt.show()
+st.pyplot(fig)
