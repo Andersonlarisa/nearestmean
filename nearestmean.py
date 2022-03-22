2645ecc2a2a3e775aeb4188ab49e31e4ad0ea7c4
Andersonlarisa <100856268+Andersonlarisa@users.noreply.github.com>
2022-03-21 22:23:44 -0300

    add proba

--- nearestmean.py
+++ nearestmean.py
@@ -12,17 +12,17 @@ class nmc(BaseEstimator, ClassifierMixin):
         self.k = k
 # Função fit faz a média dos pontos de cada classe  
     def fit(self, x_train, y_train):
-        qclasses=np.unique(y_train)
-        cluster_mean=np.zeros((qclasses.size,x_train.shape[1]))
+        classes=np.unique(y_train)
+        cluster_mean=np.zeros((classes.size,x_train.shape[1]))
         count=0
-        for i in qclasses:
+        for i in classes:  # Trocar para enumerate
             yindex=y_train==i
             xclasse=x_train[yindex]
             cluster_mean[count,:]=xclasse.mean(axis=0)
             count+=1
         
-        self.qclasses=qclasses
-        self.cluster_mean=cluster_mean
+        self.classes=classes   # array classes
+        self.cluster_mean=cluster_mean  # Posição do centro de cada classe
         self.X_train = x_train
         self.y_train = y_train
         return self
@@ -30,7 +30,7 @@ class nmc(BaseEstimator, ClassifierMixin):
 # a menor distancia determina a que classe o ponto deve pertencer .   
     def predict(self, x_test):
         m=x_test.shape[0]
-        n=self.qclasses.size
+        n=self.classes.size
         distance=np.zeros((m,n))
         for i in range(m):
             for j in range(n):
@@ -40,9 +40,35 @@ class nmc(BaseEstimator, ClassifierMixin):
                 distance[i,j]=dist
         y_pred=distance.argmin(axis=1)
                 
-        return y_pred       
+        return y_pred
     
-    #Funções necessarias para a utilização do Gridsearch e Pipeline
+    def predict_proba(self, x_test):
+        n=self.classes.size # Para determinar a probabilidade o k deve ser igual a quantidade de classes
+        m=x_test.shape[0]
+        distance=np.zeros((m,n))
+        probabilidades=[]
+
+        for i in range(m):
+            for j in range(n):
+                a=x_test[i,:]
+                b=self.cluster_mean[j,:]
+                dist = np.sqrt(np.sum(np.square(a-b)))
+                distance[i,j]=dist
+        
+        with np.errstate(divide='ignore'):
+            peso = 1. / distance
+        inf_mask = np.isinf(peso)
+        inf_row = np.any(inf_mask, axis=1)
+        peso[inf_row] = inf_mask[inf_row]
+        
+        normalizador=peso.sum(axis=1)[:, np.newaxis]
+        normalizador[normalizador == 0.0] = 1.0
+        peso /= normalizador
+        probabilidades.append(peso)
+        
+        return probabilidades
+    
+    #Funções necessarias para a utilização do Gridsearch e Pipeline.
     def score(self, x_test, y_test):
         predictions = self.predict(x_test)
         return (predictions == y_test).sum() / len(y_test)
---
+++ teste_proba.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Anderson
+"""
+
+from sklearn.datasets import make_blobs
+import numpy as np
+from nearestmean import nmc
+import matplotlib.pyplot as plt
+import streamlit as st
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestCentroid
+
+st.title('Exemplo Nearest Mean')
+
+st.sidebar.write('Parâmetros do Dataset')
+qtd_Amostras=st.sidebar.number_input('Qtd Amostras',value=100,min_value=2)
+qtd_Classes=st.sidebar.number_input('Qtd Classes',value=3,min_value=2)
+desvio=st.sidebar.number_input('Desvio Padrão',value=0.4,min_value=0.0)
+random_state=st.sidebar.number_input('Random State',value=0,min_value=0)
+
+
+x, y = make_blobs(n_samples=qtd_Amostras, centers=qtd_Classes, n_features=2,cluster_std=desvio,
+                  random_state=random_state)
+
+x_test,y_test =make_blobs(n_samples=10, centers=qtd_Classes, n_features=2,cluster_std=desvio,
+                  random_state=7)
+
+
+clf=nmc()
+clf.fit(x,y)
+
+nc=NearestCentroid()
+nc.fit(x,y)
+
+
+
+st.sidebar.write('Amostra para classificação')
+x1=st.sidebar.number_input('Amostra eixo x',value=1)
+y1=st.sidebar.number_input('Amostra eixo y',value=2)
+#x_test=np.array([x1,y1],ndmin=2)
+
+y_proba_nmc=clf.predict_proba(x_test)
+y_pred_nc=nc.predict(x_test)
+y_pred_nmc=clf.predict(x_test)
+
+#fig, ax = plt.subplots()
+#ax.scatter(x[:,0], x[:,1],c=y)
+#ax.scatter(clf.cluster_mean[:,0],clf.cluster_mean[:,1],c='#000000')
+#ax.scatter(x_test[:,0],x_test[:,1],c=y_pred,marker='x',vmin=y.min(),vmax=y.max())
+
+#plt.show()
+#st.pyplot(fig)
